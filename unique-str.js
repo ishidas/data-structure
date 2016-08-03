356779ea07656a9b4a783500d7ea6e7442b7cf69
Sawako Ishida <sawako.yoriko@gmail.com>
2016-08-03 13:03:05 -0700

    unique string check complete

---
+++ unique-str.js
@@ -0,0 +1,20 @@
+'use strict';
+const str1 = 'abcde';//unique
+const str2 = 'adeac';//dup
+
+function isUnique (str){
+  for (var i = 0; i < str.length; i++) {
+    let a = str[i];
+    let sub = str.substring(i + 1);
+    for (var j = 0; j < sub.length; j++) {
+      if(a === sub[j]) return false;
+      if(sub.length === 1) return true;
+      isUnique(sub);
+    }
+  }
+}
+
+let result1 = isUnique(str1);
+let result2 = isUnique(str2);
+console.log('result 1', result1);
+console.log('result 2', result2);
