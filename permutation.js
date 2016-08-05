de15e6c1b665dc79dad250147a1be0e7c0915a26
Sawako Ishida <sawako.yoriko@gmail.com>
2016-08-04 22:19:37 -0700

    check permutation on 2 str

---
+++ permutation.js
@@ -0,0 +1,16 @@
+'use strict';
+const str1 = 'ab!d0 e';
+const str2 = 'a0 d!eb';
+
+function checkPermutation(string1, string2){
+  let strOne = string1.split('').sort().join();
+  let strTwo = string2.split('').sort().join();
+  console.log('string One', strOne);
+  console.log('string Two', strTwo);
+  if(strOne === strTwo) return true;
+  if(strOne !== strTwo) return false;
+}
+
+
+let result = checkPermutation(str1, str2);
+console.log('result', result);
