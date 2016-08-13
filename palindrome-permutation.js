26cd521e7874951e3544751a48effaf4d21f0bdf
Sawako Ishida <sawako.yoriko@gmail.com>
2016-08-13 15:23:08 -0700

    permutation of palindrome completed ref:http://codereview.stackexchange.com/questions/77511/check-if-string-can-be-rearranged-into-a-palindrome

---
+++ palindrome-permutation.js
@@ -0,0 +1,24 @@
+'use strict';
+function permutationOfPalindrome (str){
+  let Dict = {};
+  let letter;
+  let countNum = 0;
+  for (var i = 0; i < str.length; i++) {
+    letter = str[i].toLowerCase();
+    Dict[letter] = Dict[letter] || 0;
+    Dict[letter] ++;
+  }
+
+  for (var prop in Dict) {
+    countNum += Dict[prop] % 2;
+    // console.log('prop', prop);
+    // console.log('num', Dict[prop]%2);
+  }
+  if(countNum < 2){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log('Is Permutation Of Palindrome?', permutationOfPalindrome('abCbcAfgg f'));
