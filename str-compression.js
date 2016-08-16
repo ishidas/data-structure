508d5c1f6e2027d55f0b240bb64736cbd3a7c991
Sawako Ishida <sawako.yoriko@gmail.com>
2016-08-15 20:59:41 -0700

    string compression done

---
+++ str-compression.js
@@ -0,0 +1,36 @@
+'use strict';
+// string compression if longer than original return original
+
+function compressString(str) {
+  if(str.length <= 1) return str !== ' '? str : 'empty string';
+  let Dict = {};
+  let letter;
+  let counter = 0;
+  let result = [];
+
+  for (var i = 0; i < str.length; i++) {
+    letter = str[i];
+    if(letter === ' '){
+      console.log('empty');
+    } else {
+      Dict[letter] = Dict[letter] || 0;
+      Dict[letter]++;
+    }
+  }
+  for (var prop in Dict) {
+    console.log('prop', prop);
+    result.push(prop);
+    result.push(Dict[prop]);
+    counter += Dict[prop] % 2;
+  }
+
+  if(str.length <= 3 && counter === 1){
+    return str;
+  } else {
+    console.log('arr', result);
+    return result.join('');
+  }
+}
+
+console.log(compressString('aab'));
+console.log(compressString('aa bbb cda'));
